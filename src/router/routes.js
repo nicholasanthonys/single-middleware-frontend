70ff173417aa0175dba0659dfc4f2f48dd785f46
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-04-20 21:33:15 +0700

    push back to home if go to login but authenticated

---
+++ src/router/routes.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from "../views/Login";
+import store from '../store';
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+        meta: {
+            requireAuth: true
+        }
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
+        meta: {
+            requireAuth: false,
+        }
+
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    let isAuthenticated = store.getters['auth/isLoggedIn']
+    if (to.name !== 'Login' && !isAuthenticated) next({ name: 'Login' })
+    else next()
+
+    if(to.name === "Login" && isAuthenticated){
+        next({name : "Home"})
+    }
+})
+
+export default router
