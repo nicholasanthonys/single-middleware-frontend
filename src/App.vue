9fe4f05c0e880ef22602a8b3b87734e73c825345
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-04-20 20:50:11 +0700

    add height 100% to html,body

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,16 @@
 </template>
 
 <style>
+html,body {
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
 }
 
 #nav {
