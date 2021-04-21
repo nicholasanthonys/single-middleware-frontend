4556e9d1913a428c045c947084461c121d0f6c97
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-04-21 07:43:05 +0700

    add store containers

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
-import auth from './auth.module'
 import Vue from 'vue'
 import Vuex from 'vuex'
+
+import auth from './auth.module'
+import containers from "./containers.module";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
         auth,
+        containers
     }
 });
