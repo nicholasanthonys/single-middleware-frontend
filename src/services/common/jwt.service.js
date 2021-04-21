9a718b6e46b93835d1df16183b8aaad462858f9f
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-04-21 07:38:54 +0700

    add function decode token

--- src/services/common/jwt.service.js
+++ src/services/common/jwt.service.js
@@ -8,6 +8,16 @@ export const saveToken = token => {
     setCookie(token)
 }
 
+export const decodeToken = token => {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
 export const destroyToken = () => {
     document.cookie =
         ID_TOKEN_KEY + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
