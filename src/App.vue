7cf391774c20850091960f360c025e68b246afd8
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-04-21 07:43:27 +0700

    use quasar

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,21 @@
 <template>
-  <div id="app">
-
-    <router-view/>
-  </div>
+   <router-view/>
 </template>
 
+<script>
+
+export default {
+  name: 'LayoutDefault',
+
+  components: {
+  },
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
 <style>
-html,body {
-  height: 100%;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height: 100%;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
