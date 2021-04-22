71a6e36e0fff1922823e571bd1a1bd7ad89e9381
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-04-22 08:35:59 +0700

    add editor component

---
+++ src/components/common/Editor.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-jsoneditor
+        :value="propCode"
+        :plus="false"
+        style="height:300px"
+        @error="onError"
+        :options="options"
+        @input="$emit(eventName, $event)"
+
+    />
+    <br/>
+    <p class="text-h5">Raw JSON Representation</p>
+    <pre>{{ JSON.stringify(propCode, null, '\t') }}</pre>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props : {
+    eventName : String,
+    propCode : Object
+  },
+  data(){
+    return {
+      code : this.propCode,
+      options: {
+        mode: null,
+        // onEditable: function (node) {
+        //   // console.log("node")
+        //   return true;
+        // },
+        theme: "ace/theme/tomorrow_night_bright",
+      },
+    }
+  },
+  methods : {
+    onError(err) {
+      console.log("error with editor")
+      console.log(err);
+
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
