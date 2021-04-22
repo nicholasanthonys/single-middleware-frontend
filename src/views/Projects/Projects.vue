2bf430f1216947034b147ecbe85d6ab3772c9cae
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-04-22 11:52:28 +0700

    add button to add new project

--- src/views/Projects/Projects.vue
+++ src/views/Projects/Projects.vue
@@ -1,56 +1,68 @@
 <template>
   <div class="q-pa-md">
+    <div class="column" style="height: 150px">
+      <div class="col-4">
 
-    <q-table
-        style="height: 400px"
-        title="Your Projects"
-        :data="data"
-        :columns="columns"
-        row-key="index"
-        virtual-scroll
-        :pagination.sync="pagination"
-        :rows-per-page-options="[0]"
-    >
+        <q-btn @click="$router.push({name : 'Projects.New'})"> Add New Project</q-btn>
 
-      <template v-slot:header="props">
-        <q-tr :props="props">
-          <q-th
-              v-for="col in props.cols"
-              :key="col.name"
-              :props="props"
-          >
-            {{ col.label }}
-          </q-th>
-        </q-tr>
-      </template>
-      <template v-slot:body="props">
-        <q-tr :props="props">
+      </div>
+      <div class="col-4">
 
-          <q-td
-              v-for="col in props.cols"
-              :key="col.name"
-              :props="props"
-          >
+        <q-table
+            style="height: 400px"
+            title="Your Projects"
+            :data="data"
+            :columns="columns"
+            row-key="index"
+            virtual-scroll
+            :pagination.sync="pagination"
+            :rows-per-page-options="[0]"
+        >
 
-            <p v-if="col.name !=='description' && col.name !== 'action' ">{{ col.value }}</p>
-            <div v-if="col.name === 'description'">
-              <p>{{ col.value }}
-                <q-tooltip anchor="bottom middle" self="top middle" :offset="[10, 10]">
-                  {{ props.row.description }}
-                </q-tooltip>
-              </p>
-            </div>
-            <q-icon
-                v-if="col.name==='action'"
-                size="xs"
-                name="edit"
-                @click="$router.push({name : 'Projects.Detail', params :{id : props.row.id}})"
-            />
+          <template v-slot:header="props">
+            <q-tr :props="props">
+              <q-th
+                  v-for="col in props.cols"
+                  :key="col.name"
+                  :props="props"
+              >
+                {{ col.label }}
+              </q-th>
+            </q-tr>
+          </template>
+          <template v-slot:body="props">
+            <q-tr :props="props">
 
-          </q-td>
-        </q-tr>
-      </template>
-    </q-table>
+              <q-td
+                  v-for="col in props.cols"
+                  :key="col.name"
+                  :props="props"
+              >
+
+                <p v-if="col.name !=='description' && col.name !== 'action' ">{{ col.value }}</p>
+                <div v-if="col.name === 'description'">
+                  <p>{{ col.value }}
+                    <q-tooltip anchor="bottom middle" self="top middle" :offset="[10, 10]">
+                      {{ props.row.description }}
+                    </q-tooltip>
+                  </p>
+                </div>
+                <q-icon
+                    v-if="col.name==='action'"
+                    size="xs"
+                    name="edit"
+                    @click="$router.push({name : 'Projects.Detail', params :{id : props.row.id}})"
+                />
+
+              </q-td>
+            </q-tr>
+          </template>
+        </q-table>
+
+      </div>
+
+    </div>
+
   </div>
 </template>
 
