bb73e616c432f6f5c255df0abad1902c20012f44
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-04-22 08:36:32 +0700

    add model for response/request config

---
+++ src/models/TreeConfigResponse.js
@@ -0,0 +1,69 @@
+export default        {
+    label: 'Circular Response',
+    children: [
+        {
+            label: 'General',
+            icon: 'settings',
+        },
+        {
+            label: 'Adds',
+            icon: 'add',
+            selectable : false,
+            children : [
+                {
+                    label: 'Adds.Header',
+                    header: 'generic',
+                    body: 'story',
+                    story: 'Lorem ipsum dolor sit amet.',
+                },
+                {
+                    label: 'Adds.Body',
+                    header: 'generic',
+                    body: 'story',
+                    story: 'Lorem ipsum dolor sit amet.',
+                },
+
+            ]
+        },
+        {
+            label: 'Modifies',
+            icon: 'edit',
+            selectable : false,
+            children : [
+                {
+                    label: 'Modifies.Header',
+                    header: 'generic',
+                    body: 'story',
+                    story: 'Lorem ipsum dolor sit amet.',
+                },
+                {
+                    label: 'Modifies.Body',
+                    header: 'generic',
+                    body: 'story',
+                    story: 'Lorem ipsum dolor sit amet.',
+                },
+
+            ]
+        },
+        {
+            label: 'Deletes',
+            icon: 'delete',
+            selectable : false,
+            children : [
+                {
+                    label: 'Deletes.Header',
+                    header: 'generic',
+                    body: 'story',
+                    story: 'Lorem ipsum dolor sit amet.',
+                },
+                {
+                    label: 'Deletes.Body',
+                    header: 'generic',
+                    body: 'story',
+                    story: 'Lorem ipsum dolor sit amet.',
+                },
+
+            ]
+        }
+    ]
+}
