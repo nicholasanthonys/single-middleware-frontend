2beff0233b609e63a0ec4df4092222270d0f0a4a
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-04-24 09:25:10 +0700

    add param

--- src/components/common/EditorRequestResponseConfig.vue
+++ src/components/common/EditorRequestResponseConfig.vue
@@ -28,9 +28,8 @@
           <div class="text-h4 q-mb-md">General</div>
 
           <div class="column">
-            <div class="col col-md-5">
+            <div class="col-1"   v-if="configType ==='response' ">
               <q-input
-                  v-if="configType ==='response' "
                   :value="statusCode"
                   type="number"
                   label="Status Code"
@@ -40,31 +39,74 @@
                   @input="onChangeStatusCode"
               />
 
+              <br/>
+            </div>
+            <div class="col-1"   v-if="configType ==='request' ">
+              <q-input
+
+                  filled
+                  type="text"
+                  v-model="destinationUrl"
+                  label="Destination Url *"
+                  hint="Request Destination Url"
+                  :rules="[ val => val && val.length > 0 || 'Please type something']"
+                  @input="onChangeDestinationUrlRequest"
+              />
+
+              <br/>
+            </div>
+
+            <div class="col-1"  v-if="configType ==='request' ">
+              <q-input
+
+                  filled
+                  type="text"
+                  v-model="destinationPath"
+                  label="Destination Path *"
+                  hint="Destination Url's Path"
+                  @input="onChangeDestinationPathRequest"
+              />
+              <br/>
+            </div>
+
+            <div class="col-1"     v-if="configType ==='request' ">
+              <q-select
+
+                  outlined v-model="requestMethod" :options="methodOptions" label="Request Method"
+                  hint="Request Method'"
+                  @input="onChangeRequestMethod"
+                  style="max-width: 200px"/>
+
+              <br/>
+            </div>
+
+            <div class="col-1">
+              <q-select
+
+                  outlined v-model="transform" :options="transformOptions" label="Transform"
+                  :hint="configType === 'request' ? 'Request Transform' : 'Response Transform'  "
+                  @input="onChangeTransform"
+                  style="max-width: 200px"/>
             </div>
             <br/>
-            <div class="col col-md-5">
-              <q-select outlined :value="transform" :options="transformOptions" label="Transform"
-                        :hint="configType === 'request' ? 'Request Transform' : 'Response Transform'  "
-                        @input="onChangeTransform"
-                        style="max-width: 200px"/>
-            </div>
-            <br/>
-            <div class="row">
-              <div class="col-12 col-lg-6">
-                <p class="text-h5">Log Before Modify</p>
-                <Editor :prop-code="logBeforeModify" event-name="on-change-log-before-modify" @on-change-log-before-modify="onChangeLogBeforeModify"/>
-              </div>
-              <div class="col-12 col-lg-6">
-                <p class="text-h5">Log After Modify</p>
-                <Editor :prop-code="logAfterModify" event-name="on-change-log-after-modify" @on-change-log-after-modify="onChangeLogAfterModify"/>
+            <div class="col-2">
+              <div class="row">
+                <div class="col-12 col-lg-6">
+                  <p class="text-h5">Log Before Modify</p>
+                  <Editor :prop-code="logBeforeModify" event-name="on-change-log-before-modify"
+                          @on-change-log-before-modify="onChangeLogBeforeModify"/>
+                </div>
+                <div class="col-12 col-lg-6">
+                  <p class="text-h5">Log After Modify</p>
+                  <Editor :prop-code="logAfterModify" event-name="on-change-log-after-modify"
+                          @on-change-log-after-modify="onChangeLogAfterModify"/>
+                </div>
               </div>
             </div>
           </div>
 
 
         </q-tab-panel>
-
-
 
 
         <q-tab-panel name="Adds.Header">
@@ -82,6 +124,12 @@
           <div class="text-h4 q-mb-md">Add Query</div>
           <Editor :propCode="codeAddQuery" :event-name="'on-change-add-query'"
                   @on-change-add-query="onChangeAddQuery"/>
+        </q-tab-panel>
+
+        <q-tab-panel name="Adds.Param" v-if="configType === 'request' ">
+          <div class="text-h4 q-mb-md">Add Param</div>
+          <Editor :propCode="codeAddParam" :event-name="'on-change-add-param'"
+                  @on-change-add-param="onChangeAddParam"/>
         </q-tab-panel>
 
         <q-tab-panel name="Modifies.Header">
@@ -102,6 +150,12 @@
                   @on-change-modify-query="onChangeModifyQuery"/>
         </q-tab-panel>
 
+        <q-tab-panel name="Modifies.Param" v-if="configType === 'request' ">
+          <div class="text-h4 q-mb-md">Modify Param</div>
+          <Editor :propCode="codeModifyParam" :event-name="'on-change-modify-param'"
+                  @on-change-modify-param="onChangeModifyParam"/>
+        </q-tab-panel>
+
         <q-tab-panel name="Deletes.Header">
           <div class="text-h4 q-mb-md">Delete Header</div>
           <Editor :propCode="codeDeleteHeader" :event-name="'on-change-delete-header'"
@@ -118,6 +172,12 @@
           <div class="text-h4 q-mb-md">Delete Query</div>
           <Editor :propCode="codeDeleteQuery" :event-name="'on-change-delete-query'"
                   @on-change-delete-query="onChangeDeleteQuery"/>
+        </q-tab-panel>
+
+        <q-tab-panel name="Deletes.Param" v-if="configType === 'request' ">
+          <div class="text-h4 q-mb-md">Delete Param</div>
+          <Editor :propCode="codeDeleteParam" :event-name="'on-change-delete-param'"
+                  @on-change-delete-param="onChangeDeleteParam"/>
         </q-tab-panel>
 
       </q-tab-panels>
@@ -137,19 +197,28 @@ export default {
     'propCodeAddHeader',
     'propCodeAddBody',
     'propCodeAddQuery',
+    'propCodeAddParam',
+
     'propCodeModifyHeader',
     'propCodeModifyBody',
     'propCodeModifyQuery',
+    'propCodeModifyParam',
+
     'propCodeDeleteHeader',
     'propCodeDeleteBody',
     'propCodeDeleteQuery',
+    'propCodeDeleteParam',
 
     'propStatusCode',
     'propTransform',
     'propLogBeforeModify',
     'propLogAfterModify',
 
-    ],
+    'propRequestMethod',
+    'propDestinationUrl',
+    'propDestinationPath'
+
+  ],
   components: {Editor},
   watch: {
     // propCodeAddHeader(val) {
@@ -158,30 +227,54 @@ export default {
   },
   data() {
     return {
-      splitterModel : 20,
+      splitterModel: 20,
       selected: "General",
+
+      requestMethod: this.propRequestMethod,
+      destinationUrl: this.propDestinationUrl,
+      destinationPath: this.propDestinationPath,
+
       statusCode: this.propStatusCode,
       transform: this.propTransform,
-      logBeforeModify: this.logBeforeModify,
-      logAfterModify: this.logAfterModify,
+      logBeforeModify: this.propLogBeforeModify,
+      logAfterModify: this.propLogAfterModify,
+
       codeAddHeader: this.propCodeAddHeader,
       codeAddBody: this.propCodeAddBody,
       codeAddQuery: this.propCodeAddQuery,
+      codeAddParam: this.propCodeAddParam,
+
       codeModifyHeader: this.propCodeModifyHeader,
       codeModifyBody: this.propCodeModifyBody,
       codeModifyQuery: this.propCodeModifyQuery,
+      codeModifyParam: this.propCodeModifyParam,
+
       codeDeleteHeader: this.propCodeDeleteHeader,
       codeDeleteBody: this.propCodeDeleteBody,
       codeDeleteQuery: this.propCodeDeleteQuery,
+      codeDeleteParam: this.propCodeDeleteParam,
 
       simple: [
         this.configType === "request" ? TreeConfigRequest : TreeConfigCircularResponse
       ],
 
-      transformOptions: ['ToJson', 'ToXml']
+      transformOptions: ['ToJson', 'ToXml'],
+      methodOptions: ['GET', 'POST', 'PUT', 'DELETE']
     }
   },
   methods: {
+    onChangeRequestMethod(val) {
+      this.$emit('on-change-method-' + this.configType, val)
+     this.requestMethod =val
+    },
+    onChangeDestinationUrlRequest(val) {
+      this.$emit('on-change-destination-url-' + this.configType, val)
+      this.destinationUrl = val
+    },
+    onChangeDestinationPathRequest(val) {
+      this.$emit('on-change-destination-path-' + this.configType, val)
+      this.destinationPath = val
+    },
     onChangeStatusCode(val) {
       this.$emit('on-change-status-code-' + this.configType, val)
       this.statusCode = val;
@@ -198,6 +291,8 @@ export default {
       this.$emit('on-change-log-after-modify-' + this.configType, val)
       this.logAfterModify = val;
     },
+
+    /* Add header, body, query , param */
     onChangeAddHeader(val) {
       this.$emit('on-change-add-header-' + this.configType, val)
       this.codeAddHeader = val;
@@ -210,6 +305,12 @@ export default {
       this.$emit('on-change-add-query-' + this.configType, val)
       this.codeAddQuery = val
     },
+    onChangeAddParam(val) {
+      this.$emit('on-change-add-param-' + this.configType, val)
+      this.codeAddParam = val
+    },
+
+    /* Modify header, body, query, param */
     onChangeModifyHeader(val) {
       this.$emit('on-change-modify-header-' + this.configType, val)
       this.codeModifyHeader = val
@@ -222,6 +323,12 @@ export default {
       this.$emit('on-change-modify-query-' + this.configType, val)
       this.codeModifyQuery = val
     },
+    onChangeModifyParam(val) {
+      this.$emit('on-change-modify-param-' + this.configType, val)
+      this.codeModifyParam = val
+    },
+
+    /* delete header, body, query, param */
     onChangeDeleteHeader(val) {
       this.$emit('on-change-delete-header-' + this.configType, val)
       this.codeDeleteHeader = val
@@ -233,6 +340,10 @@ export default {
     onChangeDeleteQuery(val) {
       this.$emit('on-change-delete-query-' + this.configType, val)
       this.codeDeleteQuery = val
+    },
+    onChangeDeleteParam(val) {
+      this.$emit('on-change-delete-param-' + this.configType, val)
+      this.codeDeleteParam = val
     }
 
 
