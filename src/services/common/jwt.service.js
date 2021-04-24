f869c54b52079e19feb397680b63469797f4a15c
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-04-24 09:25:23 +0700

    wrap decodeToken with try catch

--- src/services/common/jwt.service.js
+++ src/services/common/jwt.service.js
@@ -9,13 +9,19 @@ export const saveToken = token => {
 }
 
 export const decodeToken = token => {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+    try {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
 
-    return JSON.parse(jsonPayload);
+        return JSON.parse(jsonPayload);
+    } catch (e) {
+        console.log(e)
+        return null;
+    }
+
 }
 
 export const destroyToken = () => {
@@ -46,4 +52,4 @@ function getCookie(cname) {
     return ''
 }
 
-export default { getToken, saveToken, destroyToken }
+export default {getToken, saveToken, destroyToken}
