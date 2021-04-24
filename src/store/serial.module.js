ac7d73c29b0404c65731d93f3dd3ca3cb921e8d8
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-04-25 01:27:59 +0700

    remove commented code

--- src/store/serial.module.js
+++ src/store/serial.module.js
@@ -46,46 +46,12 @@ const serial = {
             })
         },
 
-        // updateSerial(context, data) {
-        //     return new Promise((resolve, reject) => {
-        //         const {projectId, configures} = data
-        //         ApiService.init()
-        //         ApiService.put(`/api/v1/project/${projectId}`, {
-        //             configures
-        //         }).then(
-        //             response => {
-        //                 resolve(response)
-        //             },
-        //             error => {
-        //                 reject(error)
-        //             }
-        //         )
-        //     })
-        // },
-
-        // deleteSerial(context, id){
-        //     return new Promise((resolve, reject) => {
-        //         ApiService.delete(`/api/v1/project/${id}` ).then(
-        //             response => {
-        //                 context.commit('deleteSerial', id)
-        //                 resolve(response)
-        //             },
-        //             error => {
-        //                 reject(error)
-        //             }
-        //         )
-        //     })
-        // }
     },
 
     mutations: {
         setSerial(state, data) {
             state.serial = data
         },
-        // deleteSerial(state ){
-        //     state.serial = null
-        // },
-
     },
 }
 
